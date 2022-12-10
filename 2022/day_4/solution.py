e2da6ee5f971172c6ce191d687b2a472427838f4
Alec Swift <aleceswift@gmail.com>
2022-12-10 15:17:30 -0800

    Add overlap function

--- 2022/day_4/solution.py
+++ 2022/day_4/solution.py
@@ -1,14 +1,43 @@
-
 from itertools import pairwise
 import re
 from typing import TextIO
 
-def parse(input_file: str) -> list[tuple[tuple[str, str]]]:
-    in_file: TextIO = open(input_file, 'r', encoding = 'utf-8')
-    with open(input_file, encoding = 'utf-8') as in_file:
+
+def parse(input_file: str) -> list[tuple[str, str]]:
+    in_file: TextIO = open(input_file, "r", encoding="utf-8")
+    with open(input_file, encoding="utf-8") as in_file:
         input_data: str = in_file.read()
-    split_ranges: list[tuple[str, str]] = re.findall(r'(\d+)-(\d+)', input_data)
-    parsed_data: list[tuple[tuple[str, str]]] = list(pairwise(split_ranges))
+    parsed_data: list[tuple[str, str]] = re.findall(r"(\d+)-(\d+)", input_data)
     return parsed_data
 
-print(parse('2022/day_4/input.txt'))
+
+def overlap(input_file: str) -> int:
+    sections: list[tuple[str, str]] = parse(input_file)
+    cp_sections: list[tuple[str, str]] = sections
+    total: int = 0
+
+    for _ in range(len(sections) // 2):
+        elf_1: tuple[str, str]
+        elf_2: tuple[str, str]
+        elf_1, elf_2, *rest = cp_sections
+
+        num_1: str
+        num_2: str
+        num_3: str
+        num_4: str
+        num_1, num_2 = elf_1
+        num_3, num_4 = elf_2
+
+        cp_sections = rest
+        if set(range(int(num_1), int(num_2) + 1)).issubset(
+            set(range(int(num_3), int(num_4) + 1))
+        ):
+            total += 1
+        elif set(range(int(num_1), int(num_2) + 1)).issuperset(
+            set(range(int(num_3), int(num_4) + 1))
+        ):
+            total += 1
+    return total
+
+
+print(parse("2022/day_4/input.txt"))
