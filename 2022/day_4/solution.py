ef868cb545deb4dc0c487ccd2bee490c984028b1
Alec Swift <aleceswift@gmail.com>
2022-12-10 15:24:48 -0800

    Add overlap_2 function

--- 2022/day_4/solution.py
+++ 2022/day_4/solution.py
@@ -51,5 +51,30 @@ def overlap(input_file: str) -> int:
             total += 1
     return total
 
+def overlap_2(input_file: str) -> int:
+    """
+    Return the sum of sections that overlap partially in
+    pairs of elves from a given input file
+    """
+    sections: list[tuple[str, str]] = parse(input_file)
+    cp_sections: list[tuple[str, str]] = sections
+    total: int = 0
 
-print(overlap("2022/day_4/input.txt"))
+    for _ in range(len(sections) // 2):
+        elf_1: tuple[str, str]
+        elf_2: tuple[str, str]
+        elf_1, elf_2, *rest = cp_sections
+
+        num_1: str
+        num_2: str
+        num_3: str
+        num_4: str
+        num_1, num_2 = elf_1
+        num_3, num_4 = elf_2
+
+        cp_sections = rest
+        if set(range(int(num_1), int(num_2) + 1)).intersection(
+            set(range(int(num_3), int(num_4) + 1))
+        ):
+            total += 1
+    return total
