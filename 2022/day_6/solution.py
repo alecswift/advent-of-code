849af761156ed69c0d1778d011079d8c6c527fdb
Alec Swift <aleceswift@gmail.com>
2022-12-10 20:27:46 -0800

    Add sub_routine function

--- 2022/day_6/solution.py
+++ 2022/day_6/solution.py
@@ -5,3 +5,12 @@ def parse(input_file: str) -> str:
     with open(input_file, encoding = 'utf-8') as in_file:
         input_data: str = in_file.read()
     return input_data
+
+def sub_routine(input_file: str) -> int:
+    buffer: str = parse(input_file)
+    for num in range(len(buffer) - 3):
+        sequence: str = buffer[num: num + 4]
+        if len(set(sequence)) == 4:
+            marker_location: int = num + 4
+            break
+    return marker_location
