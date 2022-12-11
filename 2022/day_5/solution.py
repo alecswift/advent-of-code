3d6321ca7189c3bb18d7558bc994c0cba35c44e4
Alec Swift <aleceswift@gmail.com>
2022-12-10 20:00:51 -0800

    Add rearrange_2 function

--- 2022/day_5/solution.py
+++ 2022/day_5/solution.py
@@ -1,3 +1,7 @@
+"""
+Find the top crates after rearranging a diagram of crates
+with given procedures both from an input file
+"""
 import re
 from typing import TextIO
 
@@ -28,6 +32,7 @@ def parse(input_file: str) -> tuple[list[list[str]], list[tuple[str, str, str]]]
             count += 1
     return parsed_diagram, parsed_proc
 
+
 def rearrange(input_file: str) -> str:
     """
     Return the rearranged diagram with the given input_file that
@@ -41,9 +46,31 @@ def rearrange(input_file: str) -> str:
         stack_from: str
         stack_to: str
         num_crates, stack_from, stack_to = line
-        move: list[str] = diagram[int(stack_from) - 1][:-int(num_crates) - 1:-1]
-        diagram[int(stack_from) - 1] = diagram[int(stack_from) - 1][0:-int(num_crates)]
+        move: list[str] = diagram[int(stack_from) - 1][: -int(num_crates) - 1 : -1]
+        diagram[int(stack_from) - 1] = diagram[int(stack_from) - 1][
+            0 : -int(num_crates)
+        ]
         diagram[int(stack_to) - 1] += move
     return "".join([column[-1] for column in diagram])
 
-print(rearrange("/home/alec/Desktop/code/advent_of_code/2022/day_5/input.txt"))
+
+def rearrange_2(input_file: str) -> str:
+    """
+    Return the rearranged diagram with the given input_file that
+    includes the original diagram and the procedures. Changed to
+    reverse the order of the moving crates
+    """
+    diagram: list[list[str]]
+    procedure: list[tuple[str, str, str]]
+    diagram, procedure = parse(input_file)
+    for line in procedure:
+        num_crates: str
+        stack_from: str
+        stack_to: str
+        num_crates, stack_from, stack_to = line
+        move: list[str] = diagram[int(stack_from) - 1][: -int(num_crates) - 1 : -1]
+        diagram[int(stack_from) - 1] = diagram[int(stack_from) - 1][
+            0 : -int(num_crates)
+        ]
+        diagram[int(stack_to) - 1] += move[::-1]
+    return "".join([column[-1] for column in diagram])
