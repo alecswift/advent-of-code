4c9fadad95588ab1c9f7064e61537ab2ae963aa4
Alec Swift <aleceswift@gmail.com>
2022-12-10 20:25:42 -0800

    Add parse function

---
+++ 2022/day_6/solution.py
@@ -0,0 +1,7 @@
+from typing import TextIO
+
+def parse(input_file: str) -> str:
+    in_file: TextIO = open(input_file, 'r', encoding = 'utf-8')
+    with open(input_file, encoding = 'utf-8') as in_file:
+        input_data: str = in_file.read()
+    return input_data
