d393f8c24bd42676657cbf7390a9f76d0477d739
Alec Swift <aleceswift@gmail.com>
2022-12-17 13:34:11 -0800

    Add list_parser function

--- 2022/day_13/solution.py
+++ 2022/day_13/solution.py
@@ -1,7 +1,15 @@
 from typing import TextIO
+from collections import deque
 
+class Node:
 
-def parse(input_file: str):
+    def __init__(self, data):
+        self.data = data
+        self.parent = None
+        self.children = [] # contains leaves and nodes
+        
+
+def split(input_file: str):
     """
     Returns ________________
     from a given input file
@@ -13,35 +21,45 @@ def parse(input_file: str):
     split_pairs = [line.split('\n') for line in split_lines]
     return split_pairs
 
-def build_items(items_data):
-    level = 0
-    items = {}
-    check = False
-    for index, item in enumerate(items_data):
-        if check:
-            check = False
-            continue
-        if item == '[':
-            level += 1
-            items[level] = []
-        elif item == ']':
-            level -= 1
-        elif items_data[index: index + 2].isdigit():
-            check = True
-            items[level].append(int(items_data[index: index + 2]))
-        elif item.isdigit():
-            items[level].append(int(item))
-        else:
-            continue
-    return items
+def list_parser(lst_data):
+    nodes = {}
+    remove_brackets = lst_data[1:-1]
+    root_node = Node(remove_brackets)
+    nodes[remove_brackets] = root_node
+    queue = deque([root_node])
+    digit_is_10 = False
+    inner_node = 0
+    while queue:
+        for index, item in enumerate(queue[0].data):
+            if digit_is_10:
+                digit_is_10 = False
+            elif item.isdigit():
+                if inner_node:
+                    continue
+                if (index != len(remove_brackets) -1) and (not remove_brackets[index + 1]):
+                    digit_is_10 = True
+                    queue[0].children.append(10)
+                else:
+                    queue[0].children.append(item)
+            elif item == '[':
+                if not inner_node:
+                    start_index = index
+                inner_node += 1
+            elif item == ']':
+                inner_node -= 1
+                if not inner_node:
+                    end_index = index
+                    node_lst_data = queue[0].data[start_index + 1: end_index]
+                    node = Node(node_lst_data)
+                    queue[0].children.append(node)
+                    nodes[node_lst_data] = node
+                    queue.append(node)
+        queue.popleft()
+    return nodes
 
 
-def build_pairs(input_file):
-    input_data = parse(input_file)
-    pairs = []
-    for item_1, item_2 in input_data:
-        pass
-        
+# print(list_parser('[1,[2,[3,[4,[5,6,0]]]],8,9]'))
+for node_1 in list_parser('[[4,4],4,4,4]').values():
+    print(node_1.children)
 
-print(parse('/home/alec/Desktop/code/advent_of_code/2022/day_13/input_test.txt'))
-print(build_items('[1,[2,[3,[4,[5,6,0]]]],8,9]'))
+# add to queue when you hit opening bracket [ keep track of  exiting/entering with 0 +-1 for []
