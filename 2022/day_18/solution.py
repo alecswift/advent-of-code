d3f2370e87badbc1c213445926896260bc4351f6
Alec Swift <aleceswift@gmail.com>
2022-12-23 07:56:04 -0800

    Add find connections function

--- 2022/day_18/solution.py
+++ 2022/day_18/solution.py
@@ -9,3 +9,21 @@ def parse(input_file):
     split_lines = input_data.split('\n')
     coords = [findall(r'(\d+),(\d+),(\d+)', line)[0] for line in split_lines]
     return [tuple(map(int, coord)) for coord in coords]
+
+def find_connections(input_file):
+    cubes = parse(input_file)
+    pairs = combinations(cubes, 2)
+    connections = 0
+    for cube_1, cube_2 in pairs:
+        x_coord, y_coord, z_coord = cube_1
+        x_coord_1, y_coord_1, z_coord_1 = cube_2
+        if (x_coord == x_coord_1) and (y_coord == y_coord_1) and (abs(z_coord - z_coord_1) == 1):
+            connections += 1
+        elif (x_coord == x_coord_1) and (abs(y_coord - y_coord_1) == 1) and (z_coord == z_coord_1):
+            connections += 1
+        elif (abs(x_coord - x_coord_1) == 1) and (y_coord == y_coord_1) and (z_coord == z_coord_1):
+            connections += 1
+    return (len(cubes) * 6) - (connections * 2)
+
+
+print(find_connections('/home/alec/Desktop/code/advent_of_code/2022/day_18/input.txt'))
