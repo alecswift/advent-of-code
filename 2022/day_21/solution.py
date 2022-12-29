86288a4f369a9eb3ceff4065049595624d6349c5
Alec Swift <aleceswift@gmail.com>
2022-12-28 23:15:38 -0800

    Add solution part 2

--- 2022/day_21/solution.py
+++ 2022/day_21/solution.py
@@ -1,10 +1,4 @@
-# start at root
-# go all the way left
-# save leaf to queue
-# go to parent of leaf and save operator to queue
-# go full left down parent
-# start from root and recursively perform operator function on children
-# depth first traversal to get postfix expression
+# All i need to do is replace sibling of x with evaled number
 from collections import deque
 from re import findall
 
@@ -23,7 +17,6 @@ class Tree:
         self.nodes = {}
         self.insert_nodes()
         self.post_fix = []
-        self.post_fix_expr(self.nodes['root'])
 
     def parse(self):
         in_file = open(self.input_file, "r", encoding="utf-8")
@@ -67,7 +60,60 @@ def eval(post_fix_expr):
             queue.append(operation(element, num_1, num_2))
     return int(queue[0])
 
-
-
 tree = Tree('2022/day_21/input.txt')
+root = tree.nodes['root']
+tree.post_fix_expr(root)
 print(eval(tree.post_fix))
+tree.post_fix = []
+
+root.value = '='
+tree.nodes['humn'].value = 'y'
+right_tree_root = tree.nodes[root.children[1]]
+left_tree_root = tree.nodes[root.children[0]]
+tree.post_fix_expr(right_tree_root)
+
+right_value = eval(tree.post_fix)
+queue_1 = deque([left_tree_root])
+while queue_1[0].value != 'y':
+    left_or_right = None
+    left, right = queue_1[0].children
+    tree.post_fix = []
+    tree.post_fix_expr(tree.nodes[left])
+    if 'y' not in  tree.post_fix:
+        tree.nodes[left].value = eval(tree.post_fix)
+        tree.nodes[left].children = []
+        left_or_right = 'right'
+    tree.post_fix = []
+    tree.post_fix_expr(tree.nodes[right])
+    if 'y' not in  tree.post_fix:
+        tree.nodes[right].value = eval(tree.post_fix)
+        tree.nodes[right].children = []
+        left_or_right = 'left'
+    operator = None
+    match queue_1[0].value:
+        case '+':
+            operator = '-'
+        case '-':
+            operator = '+'
+        case '*':
+            operator = '/'
+        case '/':
+            operator = '*'
+    if left_or_right == 'left':
+        queue_1.append(tree.nodes[left])
+        right_value = operation(operator, right_value, tree.nodes[right].value)
+    if left_or_right == 'right':
+        if operator == '+':
+            queue_1.append(tree.nodes[right])
+            right_value = operation('-', tree.nodes[left].value, right_value)
+        elif operator == '*':
+            queue_1.append(tree.nodes[right])
+            right_value = operation('/', tree.nodes[left].value, right_value)
+        else:
+            queue_1.append(tree.nodes[right])
+            right_value = operation(operator, right_value, tree.nodes[left].value)
+    queue_1.popleft()
+print(right_value)
+
+
+
