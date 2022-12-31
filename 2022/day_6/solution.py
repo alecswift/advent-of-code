63204f54b5036ffb1836ef57346582ef94577c6b
Alec Swift <aleceswift@gmail.com>
2022-12-30 19:05:31 -0800

    Combine sub_routine and sub_routine_2 function

--- 2022/day_6/solution.py
+++ 2022/day_6/solution.py
@@ -15,29 +15,17 @@ def parse(input_file: str) -> str:
         input_data: str = in_file.read()
     return input_data
 
-def sub_routine(input_file: str) -> int:
+def sub_routine(input_file: str, num_digits: int) -> int:
     """
     return a marker location for 4 distinct characters from an input file
     """
     buffer: str = parse(input_file)
-    for num in range(len(buffer) - 3):
-        sequence: str = buffer[num: num + 4]
-        if len(set(sequence)) == 4:
-            marker_location: int = num + 4
+    for num in range(len(buffer) - (num_digits - 1)):
+        sequence: str = buffer[num: num + num_digits]
+        if len(set(sequence)) == num_digits:
+            marker_location: int = num + num_digits
             break
     return marker_location
 
-def sub_routine_2(input_file: str) -> int:
-    """
-    return a marker location for 14 distinct characters from an input file
-    """
-    buffer: str = parse(input_file)
-    for num in range(len(buffer) - 13):
-        sequence: str = buffer[num: num + 14]
-        if len(set(sequence)) == 14:
-            marker_location: int = num + 14
-            break
-    return marker_location
-
-print(sub_routine('2022/day_6/input.txt'))
-print(sub_routine_2('2022/day_6/input.txt'))
+print(sub_routine('2022/day_6/input.txt', 4))
+print(sub_routine('2022/day_6/input.txt', 14))
