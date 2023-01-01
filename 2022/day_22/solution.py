ed7c3b6a107a96ae3d44240b0debc24cc75e1d64
Alec Swift <aleceswift@gmail.com>
2023-01-01 15:39:27 -0800

    Add wrapper function

--- 2022/day_22/solution.py
+++ 2022/day_22/solution.py
@@ -18,6 +18,7 @@ def initial_parse(input_file):
 def parse(grid):
     rows = [list(row) for row in grid]
     max_x = max([len(row) for row in rows])
+    # Add white spaces to the end of rows to equalize lengths
     for row in rows:
         length = len(row)
         if length < max_x:
@@ -25,6 +26,36 @@ def parse(grid):
     columns = array(rows).transpose().tolist()
     return rows, columns
 
+def wrapper(rows):
+    walls = set()
+    start = False
+    row_wrappers = {}
+    for y_coord, row in zip(range(len(rows) -1, -1, -1), rows):
+        for x_coord, char in enumerate(row):
+            if char != ' ':
+                if not start:
+                    start = True
+                    start_coord = complex(x_coord, y_coord)
+                if char == '#':
+                    walls.add(complex(x_coord, y_coord))
+            elif start:
+                end_coord = complex(x_coord, y_coord)
+                row_wrappers[start_coord] = end_coord
+                start = False
+                break
+        else:
+            end_coord = complex(len(row) - 1, y_coord)
+            row_wrappers[start_coord] = end_coord
+            start = False
+    # add column checker to flip x and y
+    # add reverse of keys/values to wrapper dict
+    # initialize wrapper dict of rows and columns 
+    # write algo for movement
+    return row_wrappers, walls
+
+
+
 
 grid_1, procedure_1 = initial_parse("2022/day_22/input_test.txt")
-print(parse(grid_1))
+rows_1, columns_1 = parse(grid_1)
+print(wrapper(rows_1))
