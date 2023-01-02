b27d27aa050b5925523a9922b91abe6db251bda0
Alec Swift <aleceswift@gmail.com>
2023-01-01 19:01:57 -0800

    Add execute proc function

--- 2022/day_22/solution.py
+++ 2022/day_22/solution.py
@@ -1,3 +1,4 @@
+from collections import deque
 from re import findall
 from numpy import array
 
@@ -26,64 +27,107 @@ def parse(grid):
     columns = array(rows).transpose().tolist()
     return rows, columns
 
+
 def wrapper(rows):
     walls = set()
     start = False
-    row_wrappers = {}
+    west_wrappers = {}
     # While iterating through characters in grid: add wall coordinates to a set
     # and connect the start of nonempty spaces to the end in a wrapper dict
-    for y_coord, row in zip(range(len(rows) -1, -1, -1), rows):
+    for y_coord, row in zip(range(len(rows) - 1, -1, -1), rows):
         for x_coord, char in enumerate(row):
-            if char != ' ':
+            if char != " ":
                 if not start:
                     start = True
                     start_coord = complex(x_coord, y_coord)
-                if char == '#':
+                if char == "#":
                     walls.add(complex(x_coord, y_coord))
             elif start:
                 end_coord = complex(x_coord - 1, y_coord)
-                row_wrappers[start_coord] = end_coord
+                west_wrappers[start_coord] = end_coord
                 start = False
                 break
         else:
             end_coord = complex(len(row) - 1, y_coord)
-            row_wrappers[start_coord] = end_coord
+            west_wrappers[start_coord] = end_coord
             start = False
 
-    for key, value in list(row_wrappers.items()):
-        row_wrappers[value] = key
-    for key in row_wrappers:
+    east_wrappers = {}
+    for key, value in list(west_wrappers.items()):
+        east_wrappers[value] = key
+    for key in west_wrappers:
         start_point = key
         break
-    return len(row_wrappers), len(walls), start_point
+    return west_wrappers, east_wrappers, walls, start_point
+
 
 def wrapper_col(columns):
     start = False
-    col_wrappers = {}
+    north_wrappers = {}
     # Same as previous function except for columns
     for x_coord, column in enumerate(columns):
-        for y_coord, char in zip(range(len(column) -1, -1, -1), column):
-            if char != ' ':
+        for y_coord, char in zip(range(len(column) - 1, -1, -1), column):
+            if char != " ":
                 if not start:
                     start = True
                     start_coord = complex(x_coord, y_coord)
             elif start:
                 end_coord = complex(x_coord, y_coord + 1)
-                col_wrappers[start_coord] = end_coord
+                north_wrappers[start_coord] = end_coord
                 start = False
                 break
         else:
             end_coord = complex(x_coord, 0)
-            col_wrappers[start_coord] = end_coord
+            north_wrappers[start_coord] = end_coord
             start = False
 
-    for key, value in list(col_wrappers.items()):
-        col_wrappers[value] = key
-    return len(col_wrappers)
+    south_wrappers = {}
+    for key, value in list(north_wrappers.items()):
+        south_wrappers[value] = key
+    return north_wrappers, south_wrappers
 
- 
+
+def execute_proc(start_point, walls, wrappers, procedure):
+    north_wrappers, south_wrappers, west_wrappers, east_wrappers = wrappers
+    directions = deque([1, -1j, -1, 1j])
+    prev_point = None
+    current_point = start_point
+    for item in procedure:
+        if isinstance(item, int):
+            for _ in range(item):
+                if current_point in walls:
+                    current_point = prev_point
+                    break
+                if (directions[0] == 1) and (current_point in east_wrappers):
+                    prev_point = current_point
+                    current_point = east_wrappers[current_point]
+                elif (directions[0] == -1) and (current_point in west_wrappers):
+                    prev_point = current_point
+                    current_point = west_wrappers[current_point]
+                elif (directions[0] == 1j) and (current_point in north_wrappers):
+                    prev_point = current_point
+                    current_point = north_wrappers[current_point]
+                elif (directions[0] == -1j) and (current_point in south_wrappers):
+                    prev_point = current_point
+                    current_point = south_wrappers[current_point]
+                else:
+                    prev_point = current_point
+                    current_point += directions[0]
+        else:
+            if item == "L":
+                directions.rotate()
+            else:
+                directions.rotate(-1)
+    return current_point, directions[0]
+
     # write algo for movement
+
+
 grid_1, procedure_1 = initial_parse("2022/day_22/input_test.txt")
 rows_1, columns_1 = parse(grid_1)
-print(wrapper(rows_1))
-print(wrapper_col(columns_1))
+west_wrappers_1, east_wrappers_1, walls_1, start_point_1 = wrapper(rows_1)
+north_wrappers_1, south_wrappers_1 = wrapper_col(columns_1)
+wrappers_1 = north_wrappers_1, south_wrappers_1, west_wrappers_1, east_wrappers_1
+final_coord, facing = execute_proc(start_point_1, walls_1, wrappers_1, procedure_1)
+print(facing)
+print(((final_coord.real + 1)*4) + ((len(rows_1) - final_coord.imag) * 1000) + 0)
