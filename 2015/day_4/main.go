a62c8531bad23d121ef129696d8743bf303da407
Alec Swift <aleceswift@gmail.com>
2023-01-12 01:30:55 -0800

    Add solutions

---
+++ 2015/day_4/main.go
@@ -0,0 +1,37 @@
+// Puzzle explanation: https://adventofcode.com/2015/day/4
+
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+func main() {
+	key := "yzbqklnj"
+	hash := findHash(key, 15)
+	hash_2 := findHash(key, 0)
+	fmt.Printf("Here's an MD5 hash that starts with 5 zeroes for your key: %d\n", hash)
+	fmt.Printf("Here's one that starts with 6 zeroes: %d", hash_2)
+}
+
+func findHash(key string, condition byte) int {
+	num := 1
+	hash := makeHash(key, num)
+	for i := 1; !(hash[0] == 0 && hash[1] == 0 && hash[2] <= condition); i++ {
+		num++
+		hash = makeHash(key, num)
+	}
+	return num
+}
+
+func makeHash(key string, num int) []byte {
+	numStr := strconv.Itoa(num)
+	hash := md5.New()
+	io.WriteString(hash, key)
+	io.WriteString(hash, numStr)
+	return hash.Sum(nil)
+	// make this return a different type?
+}
