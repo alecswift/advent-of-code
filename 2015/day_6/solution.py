ba152fa2548a410137dde4578eeb8a11ce8694ef
Alec Swift <aleceswift@gmail.com>
2023-01-12 23:43:08 -0800

    Split functions into part 1 and part 2

--- 2015/day_6/solution.py
+++ 2015/day_6/solution.py
@@ -1,13 +1,18 @@
+"""Puzzle explanation: https://adventofcode.com/2015/day/6"""
+
 from re import findall, split
+from itertools import product
+
 
 def main():
     instructions, intervals = parse("day_6/input.txt")
     print(light_show(instructions, intervals))
-    print(light_show_2(instructions, intervals))
+    print(sum(lights_on_2.values()))
+
 
 def parse(input_file):
-    in_file = open(input_file, "r", encoding = "utf-8")
-    with open(input_file, encoding = "utf-8") as in_file:
+    in_file = open(input_file, "r", encoding="utf-8")
+    with open(input_file, encoding="utf-8") as in_file:
         input_data = in_file.read()
     split_lines = split(r"\n", input_data)
 
@@ -25,47 +30,42 @@ def parse(input_file):
         instructions.append(instruction)
     return instructions, intervals
 
+# make this whole module a class
+lights_on_2 = {}
+lights_on = set()
+
 def light_show(instructions, intervals):
-    lights_on = set()
     for index, interval in enumerate(intervals):
+        instruction = instructions[index]
         x_1, y_1, x_2, y_2 = interval
-        for x_coord in range(x_1, x_2 + 1):
-            for y_coord in range(y_1, y_2 + 1):
-                coord = complex(x_coord, y_coord)
-                if instructions[index] == 1:
-                    if coord not in lights_on:
-                        lights_on.add(coord)
-                elif instructions[index] == 0:
-                    if coord in lights_on:
-                        lights_on.remove(coord)
-                else:
-                    if coord in lights_on:
-                        lights_on.remove(coord)
-                    else:
-                        lights_on.add(coord)
+        for x_coord, y_coord in product(range(x_1, x_2 + 1), range(y_1, y_2 + 1)):
+            coord = complex(x_coord, y_coord)
+            part_1(instruction, coord)
+            part_2(instruction, coord)
     return len(lights_on)
 
-def light_show_2(instructions, intervals):
-    lights_on = {}
-    for index, interval in enumerate(intervals):
-        x_1, y_1, x_2, y_2 = interval
-        for x_coord in range(x_1, x_2 + 1):
-            for y_coord in range(y_1, y_2 + 1):
-                coord = complex(x_coord, y_coord)
-                if instructions[index] == 1:
-                    lights_on[coord] = lights_on.setdefault(coord, 0) + 1
-                elif instructions[index] == 0:
-                    if lights_on.get(coord):
-                        lights_on[coord] -= 1
-                else:
-                    lights_on[coord] = lights_on.setdefault(coord, 0) + 2
-    return sum(lights_on.values())
+def part_1(instruction, coord):
+    if instruction == 1:
+        if coord not in lights_on:
+            lights_on.add(coord)
+    elif instruction == 0:
+        if coord in lights_on:
+            lights_on.remove(coord)
+    else:
+        if coord in lights_on:
+            lights_on.remove(coord)
+        else:
+            lights_on.add(coord)
+
+def part_2(instruction, coord):
+    if instruction == 1:
+        lights_on_2[coord] = lights_on_2.setdefault(coord, 0) + 1
+    elif instruction == 0:
+        if lights_on_2.get(coord):
+            lights_on_2[coord] -= 1
+    else:
+        lights_on_2[coord] = lights_on_2.setdefault(coord, 0) + 2
 
 
-
-
-        
 if __name__ == "__main__":
     main()
-
-    
