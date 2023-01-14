6531271b9e5edfb3c646edee05581aac4a67d676
Alec Swift <aleceswift@gmail.com>
2023-01-13 17:12:27 -0800

    Add comments

--- 2015/day_7/solution.py
+++ 2015/day_7/solution.py
@@ -4,46 +4,49 @@ from collections import deque
 from re import findall, split
 
 def main():
-    wires, operations = parse("2015/day_7/input.txt")
-    wires_1, operations_1 = parse("2015/day_7/input_2.txt")
-    print(emulate_circuit(wires, operations))
-    print(emulate_circuit(wires_1, operations_1))
+    wires, instructions = parse("2015/day_7/input.txt")
+    wires_1, instructions_1 = parse("2015/day_7/input_2.txt")
+    print(emulate_circuit(wires, instructions))
+    print(emulate_circuit(wires_1, instructions_1))
 
 def parse(input_file):
     in_file = open(input_file, "r", encoding = "utf-8")
     with open(input_file, encoding = "utf-8") as in_file:
         input_data = in_file.read()
     split_lines = split(r"\n", input_data)
+    # Wire names mapped to their signal
     wires = {}
-    operations = []
+    # The instructions to build the circuit: operands, operator, result
+    instructions = []
     for line in split_lines:
         wire_names = findall(r"[a-z]+", line)
         wires.update({name: None for name in wire_names})
-        
+
         operator = findall(r"[A-Z]+", line)
-        if not operator: # this is the start or end wire
+        if not operator:
             source, dest = findall(r"\d+|[a-z]+", line)
             if source.isdigit():
                 source = int(source)
-            operations.append(([source], "=", dest))
+            instructions.append(([source], "=", dest))
             continue
         operator, = operator
         if operator == "NOT":
             source, dest = wire_names
-            operations.append(([source], operator, dest))
+            instructions.append(([source], operator, dest))
         else:
             nums = findall(r"\d+", line)
             if nums:
                 num, = nums
                 source, dest = wire_names
-                operations.append(([source, int(num)], operator, dest))
+                instructions.append(([source, int(num)], operator, dest))
             else:
                 source_1, source_2, dest = wire_names
-                operations.append(([source_1, source_2], operator, dest))
-    return wires, operations
+                instructions.append(([source_1, source_2], operator, dest))
+    return wires, instructions
 
-def emulate_circuit(wires, operations):
-    queue = deque(operations)
+def emulate_circuit(wires, instructions):
+    queue = deque(instructions)
+    # rotate the queue of instructions until the circuit is complete
     while queue:
         sources, operator, dest = queue[0]
         nums = []
