c74bea2e8712e900c515c4e0dac9ee357026d434
Alec Swift <aleceswift@gmail.com>
2023-01-18 00:22:12 -0800

    Add solutions

---
+++ 2015/day_12/solution.py
@@ -0,0 +1,42 @@
+input_file = "2015/day_12/input.txt"
+in_file = open(input_file, "r", encoding="utf-8")
+with open(input_file, encoding="utf-8") as in_file:
+    input_data = in_file.read()
+
+current_num = ""
+total = 0
+for index, char in enumerate(input_data):
+    if char.isdigit() or char == "-":
+        current_num += char
+    elif input_data[index - 1].isdigit():
+        total += int(current_num)
+        current_num = ""
+
+print(total)
+
+def search(data, numbers = [], count = 0):
+    if isinstance(data, dict):
+        for element in data.values():
+            if isinstance(element, dict):
+                if "red" in element.values():
+                    continue
+                search(element, numbers, count)
+            if isinstance(element, list):
+                search(element, numbers, count)
+            if isinstance(element, int):
+                numbers.append(element)
+        return sum(numbers)
+    if isinstance(data, list):
+        for element in data:
+            if isinstance(element, dict):
+                if "red" in element.values():
+                    continue
+                search(element, numbers, count)
+            if isinstance(element, list):
+                search(element, numbers, count)
+            if isinstance(element, int):
+                numbers.append(element)
+        return sum(numbers)
+
+data = eval(input_data)
+print(search(data))
