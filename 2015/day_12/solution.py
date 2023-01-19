8e80c67fc425b22a172bc44fa99b9ba270057d7b
Alec Swift <aleceswift@gmail.com>
2023-01-18 23:27:19 -0800

    Modularize

--- 2015/day_12/solution.py
+++ 2015/day_12/solution.py
@@ -1,28 +1,36 @@
-input_file = "2015/day_12/input.txt"
-in_file = open(input_file, "r", encoding="utf-8")
-with open(input_file, encoding="utf-8") as in_file:
-    input_data = in_file.read()
+"""Puzzle Explanation: https://adventofcode.com/2015/day/12"""
 
-current_num = ""
-total = 0
-for index, char in enumerate(input_data):
-    if char.isdigit() or char == "-":
-        current_num += char
-    elif input_data[index - 1].isdigit():
-        total += int(current_num)
-        current_num = ""
+def main():
+    input_file = "2015/day_12/input.txt"
+    in_file = open(input_file, "r", encoding="utf-8")
+    with open(input_file, encoding="utf-8") as in_file:
+        input_data = in_file.read()
+    part_1 = add_all_numbers(input_data)
+    print(f"The total sum of all numbers is: {part_1}")
+    data = eval(input_data)
+    part_2 = search_nested_data(data)
+    print(f"The sum of all numbers with 'red' not in the same object is: {part_2}")
 
-print(total)
+def add_all_numbers(input_data):
+    current_num = ""
+    total = 0
+    for index, char in enumerate(input_data):
+        if char.isdigit() or char == "-":
+            current_num += char
+        elif input_data[index - 1].isdigit():
+            total += int(current_num)
+            current_num = ""
+    return total
 
-def search(data, numbers = []):
+def search_nested_data(data, numbers = []):
     if isinstance(data, dict):
         for element in data.values():
             if isinstance(element, dict):
                 if "red" in element.values():
                     continue
-                search(element, numbers)
+                search_nested_data(element, numbers)
             if isinstance(element, list):
-                search(element, numbers)
+                search_nested_data(element, numbers)
             if isinstance(element, int):
                 numbers.append(element)
         return sum(numbers)
@@ -31,12 +39,12 @@ def search(data, numbers = []):
             if isinstance(element, dict):
                 if "red" in element.values():
                     continue
-                search(element, numbers)
+                search_nested_data(element, numbers)
             if isinstance(element, list):
-                search(element, numbers)
+                search_nested_data(element, numbers)
             if isinstance(element, int):
                 numbers.append(element)
         return sum(numbers)
 
-data = eval(input_data)
-print(search(data))
+if __name__ == "__main__":
+    main()
