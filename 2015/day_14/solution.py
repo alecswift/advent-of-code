ddcbb1fad7c6241ee73d4b8e76ccf4b0db993b76
Alec Swift <aleceswift@gmail.com>
2023-01-21 18:45:02 -0800

    Combine race_1 and race_2 functions

--- 2015/day_14/solution.py
+++ 2015/day_14/solution.py
@@ -1,9 +1,14 @@
+"""Puzzle explanation: https://adventofcode.com/2015/day/14"""
+
 from re import findall
+
 
 def main():
     reindeers = parse("2015/day_14/input.txt")
-    print(max(race_1(reindeer, 2503) for reindeer in reindeers))
-    print(race_2(reindeers, 2503))
+    max_dist, high_score = race(reindeers, 2503)
+    print(f"For part 1, the winning reindeer traveled {max_dist} km")
+    print(f"For part 2, the winning reindeer scored {high_score} points")
+
 
 def parse(input_file):
     in_file = open(input_file, "r", encoding="utf-8")
@@ -13,16 +18,8 @@ def parse(input_file):
     reindeers = [list(map(int, findall(r"\d+", line))) for line in split_lines]
     return reindeers
 
-def race_1(reindeer, seconds):
-    speed, time, rest = reindeer
-    total_time, distance_per_cycle = time + rest, speed * time
-    num_of_cycles, remaining_time = divmod(seconds, total_time)
-    initial_distance = num_of_cycles * distance_per_cycle
-    if remaining_time < time:
-        return initial_distance + (speed * remaining_time)
-    return initial_distance + distance_per_cycle
 
-def race_2(reindeers, seconds):
+def race(reindeers, seconds):
     length = len(reindeers)
     distances = [0] * length
     scores = [0] * length
@@ -35,19 +32,21 @@ def race_2(reindeers, seconds):
         top_scores = find_top_score_positions(distances)
         for index in top_scores:
             scores[index] += 1
-    return max(scores)
+    return max(distances), max(scores)
+
 
 def find_top_score_positions(distances):
     index = 1
     maxes = [0]
     while index < len(distances):
-            if distances[maxes[0]] < distances[index]:
-                maxes = []
-                maxes.append(index)
-            elif distances[maxes[0]] == distances[index]:
-                maxes.append(index)
-            index += 1
+        if distances[maxes[0]] < distances[index]:
+            maxes = []
+            maxes.append(index)
+        elif distances[maxes[0]] == distances[index]:
+            maxes.append(index)
+        index += 1
     return maxes
+
 
 if __name__ == "__main__":
     main()
