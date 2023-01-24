faa6a8774dba11bb03cb23be74707b1e3ca74f17
Alec Swift <aleceswift@gmail.com>
2023-01-23 23:44:27 -0800

    Add solution part 2

--- 2015/day_15/solution.py
+++ 2015/day_15/solution.py
@@ -4,10 +4,12 @@ from re import findall, split
 
 def main():
     ingredients = parse("2015/day_15/input.txt")
-    combo = [0, 0, 0, 0]
-    ingredients = [ingredient[:-1] for ingredient in ingredients]
     num_of_ingredients = len(ingredients)
-    print(find_best_cookie(num_of_ingredients, 100, ingredients, combo))
+    combo = [0] * len(ingredients)
+    part_1 = find_best_cookie(num_of_ingredients, 100, ingredients, combo, True)
+    part_2 = find_best_cookie(num_of_ingredients, 100, ingredients, combo, False)
+    print(f"The best cookie scores {part_1} points")
+    print(f"The best cookie with only 500 calories scores {part_2} points")
 
 
 def parse(input_file):
@@ -21,31 +23,42 @@ def parse(input_file):
     return ingredients
 
 
-def find_best_cookie(num_of_ingredients, number, ingredients, combo):
+def find_best_cookie(num_of_ingredients, number, ingredients, combo, part_1):
     max_score = 0
     if num_of_ingredients == 1:
         combo[num_of_ingredients - 1] = number
-        return calc_score(combo, ingredients)
+        return calc_score(combo, ingredients, part_1)
     for num in range(number - 1, 0, -1):
         combo[num_of_ingredients - 1] = num
         max_score = max(
             max_score,
             find_best_cookie(
-                num_of_ingredients - 1, number - num, ingredients, deepcopy(combo)
+                num_of_ingredients - 1,
+                number - num,
+                ingredients,
+                deepcopy(combo),
+                part_1,
             ),
         )
     return max_score
 
 
-def calc_score(combo, ingredients):
+def calc_score(combo, ingredients, part_1):
     combo_score = 1
-    for column in range(len(ingredients)):
+    for column in range(len(ingredients[0])):
         curr_sum = 0
         for idx, tspoon in enumerate(combo):
             curr_sum += tspoon * ingredients[idx][column]
-        curr_sum = max(curr_sum, 0)
-        combo_score *= curr_sum
-    return combo_score
+
+        if column == len(ingredients[0]) - 1:
+            cals = curr_sum
+        else:
+            curr_sum = max(curr_sum, 0)
+            combo_score *= curr_sum
+
+    if part_1:
+        return combo_score
+    return combo_score if cals == 500 else 0
 
 
 if __name__ == "__main__":
