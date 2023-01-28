400c47237356acacacd0dd62701a4db7b3e2487e
Alec Swift <aleceswift@gmail.com>
2023-01-27 18:40:23 -0800

    Modularize code

--- 2015/day_17/solution.py
+++ 2015/day_17/solution.py
@@ -1,24 +1,33 @@
 from itertools import combinations
+from typing import TextIO
 
-with open("2015/day_17/input.txt", encoding = "utf-8") as in_file:
-    input_data = in_file.read()
 
-data = [int(num) for num in input_data.split("\n")]
+def main():
+    in_file: TextIO
+    with open("2015/day_17/input.txt", encoding = "utf-8") as in_file:
+        input_data: str = in_file.read()
+    data: list[int] = [int(num) for num in input_data.split("\n")]
+    combos, combos_min_containers = find_subsets_with_sum_of_150(data)
+    print(combos, combos_min_containers)
 
-combos = 0
-combos_min_containers = 0
-first_hit = False
-min_containers = -1
-for num in range(len(data)):
-    for combo in combinations(data, num):
-        if sum(combo) == 150:
-            combos += 1
-            if not first_hit:
-                min_containers = num
-                first_hit = True
-    if num == min_containers:
-        combos_min_containers = combos
-            
 
-print(combos)
-print(combos_min_containers)
+def find_subsets_with_sum_of_150(data):
+    combos = 0
+    combos_min_containers = 0
+    first_hit = False
+    min_containers = -1
+    # we know that there are no 3 combinations that add up to 150
+    for num in range(4, len(data)):
+        for combo in combinations(data, num):
+            if sum(combo) == 150:
+                combos += 1
+                if not first_hit:
+                    min_containers = num
+                    first_hit = True
+        if num == min_containers:
+            combos_min_containers = combos
+    return combos, combos_min_containers
+
+
+if __name__ == "__main__":
+    main()
