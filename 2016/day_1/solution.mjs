238f9affa5add4e29b3a52ce75a19af721987a8a
Alec Swift <aleceswift@gmail.com>
2023-01-31 00:19:32 -0800

    Add part 2 solution

--- 2016/day_1/solution.mjs
+++ 2016/day_1/solution.mjs
@@ -1,17 +1,21 @@
 // Puzzle Explanation: https://adventofcode.com/2016/day/1
 
+import { complex, add } from 'mathjs'
 import { readFileSync } from 'fs';
 
 const instructions = readFileSync('day_1/input.txt', 'utf8').split(", ");
 
 function solution(instructions) {
-    let position = [0, 0]
+    let part2 = false
+    let position = complex(0,0)
+    let part2Pos = null
     // North, South, East, West
+    let seen = [position]
     let orientations = [
-        [0, 1],
-        [1, 0],
-        [0, -1],
-        [-1, 0]
+        complex(0, 1),
+        complex(1, 0),
+        complex(0, -1),
+        complex(-1, 0)
     ]
     for (let instruction of instructions) {
         let direction = instruction[0]
@@ -26,13 +30,31 @@ function solution(instructions) {
                 orientations.push(bottom)
         }
         // modify position based on spaces and current orientation
-        for (let i = 0; i < position.length; i ++) {
-            if (orientations[0][i] !== 0) {
-                position[i] += (orientations[0][i] * spaces)
+        for (let j = 0; j < spaces; j ++) {
+            position = add(position, orientations[0])
+            // Find the first coordinate that appears twice
+            if (!part2) {
+                if (linearSearch(seen, position)) {
+                    part2Pos = position
+                    part2 = true
+                } else {
+                    seen.push(position)
+                }
             }
         }
     }
-    return Math.abs(position[0]) + Math.abs(position[1])
+    const part1sol = Math.abs(position.re) + Math.abs(position.im)
+    const part2sol = Math.abs(part2Pos.re) + Math.abs(part2Pos.im)
+    return [part1sol, part2sol]
+}
+
+function linearSearch(coords, target) {
+    for (let coord of coords) {
+        if (coord.equals(target)) {
+            return true
+        }
+    }
+    return false
 }
 
 console.log(solution(instructions))
