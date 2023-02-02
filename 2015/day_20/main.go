1f340e75e728ada9de6fd8ef208b686ed78335cd
Alec Swift <aleceswift@gmail.com>
2023-02-01 20:02:57 -0800

    Make directory day 20

---
+++ 2015/day_20/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	target := 36000000
+}
+
+func findHouse(target int) int {
+	seen := map[int]int{
+		1: 10,
+	}
+	numOfPresents := 0
+	for house := 2; numOfPresents < target; house++ {
+		if house % 2 == 0 {
+			
+		}
+	}
+}
