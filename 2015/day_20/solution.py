32579940ba231f28fbcb9a03b45b88eb8f0d6ec3
Alec Swift <aleceswift@gmail.com>
2023-02-01 21:32:09 -0800

    Add part 2 solution

--- 2015/day_20/solution.py
+++ 2015/day_20/solution.py
@@ -2,12 +2,12 @@
 
 def find_house(target):
     houses = {}
-    for elf in range(1, target // 25):
-        for house in range(elf, target // 25, elf):
+    for elf in range(1, target // 11):
+        for house in range(elf, 51 * elf, elf):
             if house not in houses:
-                houses[house] = elf * 10
+                houses[house] = elf * 11
             else:
-                houses[house] += elf * 10
+                houses[house] += elf * 11
         if target <= houses[elf]:
             return elf
         del houses[elf]
