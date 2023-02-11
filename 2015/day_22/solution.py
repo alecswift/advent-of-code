47a3643747413f330f8cb9ea4177e3caeb508c2f
Alec Swift <aleceswift@gmail.com>
2023-02-11 04:36:19 -0800

    Add solution part 1

--- 2015/day_22/solution.py
+++ 2015/day_22/solution.py
@@ -1,29 +1,5 @@
 """Puzzle explanation: https://adventofcode.com/2015/day/22"""
 
-# Hit Points: 58
-# Damage: 9
-
-    
-    # have to decide how to deal with turned base effects
-    # Shield, poison, recharge
-    
-# dynamic programming
-# state is data members of boss object and data members of wizard object
-# for the turned base game
-
-# subproblem to recursively implement to find all paths then find the path with the min mana
-# for move in moveset, chose move, implement turn boss and wizards data members change
-# choose another move (if a turn based move is activated keep track of the
-# turns and don't use the move again until the counter is over)
-# Repeat this process until one of the character's is dead
-# only keep track of paths where the wizard wins
-# keep track of the minimum amount of mana utilized so far and
-# only continue path if you have used less mana than the minimum
-
-# return function if move is a current prolonged effect.
-# return if one of the characters died
-# return mana is greater than the current min mana
-# states: (wizhp, bosshp, mana, visited, counters)
 
 class MinManaSpent:
 
@@ -43,99 +19,97 @@ def rec_fight_simulator(wizhp, wizarmour, mana, bosshp, visited, mana_spent, min
     shield_visited, poison_visited, recharge_visited = visited
     # currently simulating 1 fight with random moves, will iterate recursively over paths later
     for move, mana_cost in wizard_moves:
+        shield_visited_temp, poison_visited_temp, recharge_visited_temp = shield_visited, poison_visited, recharge_visited
+        temp_wizhp, temp_wizarmour, temp_mana, temp_boss_hp, temp_mana_spent = wizhp, wizarmour, mana, bosshp, mana_spent
         # wiz turn
-        if shield_visited is not None:
-            if shield_visited == 0:
-                wizarmour = 0
-                shield_visited = None
-            else:
-                shield_visited -= 1
-        if poison_visited is not None:
-            if poison_visited == 0:
-                poison_visited = None
-            else:
-                bosshp -= 3
-                poison_visited -= 1
-        if recharge_visited is not None:
-            if recharge_visited == 0:
-                recharge_visited = None
-            else:
-                mana += 101
-                recharge_visited -= 1
+        if shield_visited_temp is not None:
+            shield_visited_temp -= 1
+            if shield_visited_temp == 0:
+                temp_wizarmour = 0
+                shield_visited_temp = None
+        if poison_visited_temp is not None:
+            temp_boss_hp -= 3
+            poison_visited_temp -= 1
+            if poison_visited_temp == 0:
+                poison_visited_temp = None
+        if recharge_visited_temp is not None:
+            temp_mana += 101
+            recharge_visited_temp -= 1
+            if recharge_visited_temp == 0:
+                recharge_visited_temp = None
 
-        if mana < mana_cost:
-            return
+        if temp_mana < mana_cost:
+            continue
 
         if move == "magic_missile":
-            bosshp -= 4
-            mana -= mana_cost
-            mana_spent += mana_cost
+            temp_boss_hp -= 4
+            temp_mana -= mana_cost
+            temp_mana_spent += mana_cost
         elif move == "drain":
-            bosshp -= 2
-            wizhp += 2
-            mana -= mana_cost
-            mana_spent += mana_cost
+            temp_boss_hp -= 2
+            temp_wizhp += 2
+            temp_mana -= mana_cost
+            temp_mana_spent += mana_cost
         elif move == "shield":
-            if shield_visited is not None:
-                return
-            wizarmour += 7
-            shield_visited = 6
-            mana -= mana_cost
-            mana_spent += mana_cost
+            if shield_visited_temp is not None:
+                continue
+            temp_wizarmour += 7
+            shield_visited_temp = 6
+            temp_mana -= mana_cost
+            temp_mana_spent += mana_cost
         elif move == "poison":
-            if poison_visited is not None:
-                return
-            poison_visited = 6
-            mana -= mana_cost
-            mana_spent += mana_cost
+            if poison_visited_temp is not None:
+                continue
+            poison_visited_temp = 6
+            temp_mana -= mana_cost
+            temp_mana_spent += mana_cost
         elif move == "recharge":
-            if recharge_visited is not None:
-                return
-            recharge_visited = 5
-            mana -= mana_cost
-            mana_spent += mana_cost
+            if recharge_visited_temp is not None:
+                continue
+            recharge_visited_temp = 5
+            temp_mana -= mana_cost
+            temp_mana_spent += mana_cost
 
-        if min_inst.min_mana_spent is not None and min_inst.min_mana_spent <= mana_spent:
-            return
+        if min_inst.min_mana_spent is not None and min_inst.min_mana_spent <= temp_mana_spent:
+            continue
 
-        if bosshp <= 0:
-            if min_inst.min_mana_spent is None:
-                min_inst.min_mana_spent = mana_spent
-            elif mana_spent < min_inst.min_mana_spent:
-                min_inst.min_mana_spent = mana_spent
-            return min_inst.min_mana_spent
+
         # boss turn
-        if shield_visited is not None:
-            if shield_visited == 0:
-                wizarmour = 0
-                shield_visited = None
-            else:
-                shield_visited -= 1
-        if poison_visited is not None:
-            if poison_visited == 0:
-                poison_visited = None
-            else:
-                bosshp -= 3
-                poison_visited -= 1
-        if recharge_visited is not None:
-            if recharge_visited == 0:
-                recharge_visited = None
-            else:
-                mana += 101
-                recharge_visited -= 1
+        if shield_visited_temp is not None:
+            shield_visited_temp -= 1
+            if shield_visited_temp == 0:
+                temp_wizarmour = 0
+                shield_visited_temp = None
+        if poison_visited_temp is not None:
+            temp_boss_hp -= 3
+            poison_visited_temp -= 1
+            if poison_visited_temp == 0:
+                poison_visited_temp = None
+        if recharge_visited_temp is not None:
+            temp_mana += 101
+            recharge_visited_temp -= 1
+            if recharge_visited_temp == 0:
+                recharge_visited_temp = None
 
-        if 9 <= wizarmour:
-            wizhp -= 1
+        if temp_boss_hp <= 0:
+            if min_inst.min_mana_spent is None:
+                min_inst.min_mana_spent = temp_mana_spent
+            elif temp_mana_spent < min_inst.min_mana_spent:
+                min_inst.min_mana_spent = temp_mana_spent
+            return min_inst.min_mana_spent
+
+        if 9 <= temp_wizarmour:
+            temp_wizhp -= 1
         else:
-            wizhp -= (8 - wizarmour)
+            temp_wizhp -= (9 - temp_wizarmour)
 
-        if wizhp <= 0:
-            return None
+        if temp_wizhp <= 0:
+            continue
 
-        visited = (shield_visited, poison_visited, recharge_visited)
-        rec_fight_simulator(wizhp, wizarmour, mana, bosshp, visited, mana_spent, min_inst)
+        temp_visited = (shield_visited_temp, poison_visited_temp, recharge_visited_temp)
+        rec_fight_simulator(temp_wizhp, temp_wizarmour, temp_mana, temp_boss_hp, temp_visited, temp_mana_spent, min_inst)
 
     return min_inst.min_mana_spent
 
 min_mana_spent_my_input = MinManaSpent()
-print(rec_fight_simulator(10, 0, 250, 13, (None, None, None), 0, min_mana_spent_my_input))
+print(rec_fight_simulator(50, 0, 500, 58, (None, None, None), 0, min_mana_spent_my_input))
