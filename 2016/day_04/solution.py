8e6efebff8afec46f610b0d882ffbcc4c236e8b1
Alec Swift <aleceswift@gmail.com>
2023-02-13 01:13:36 -0800

    Add solution part 2

--- 2016/day_04/solution.py
+++ 2016/day_04/solution.py
@@ -1,9 +1,8 @@
 from collections import Counter, deque
 from re import findall
-from strings import ascii_lower
+from string import ascii_lowercase
 from typing import TextIO
 
-RoomCodes = list[tuple[str, str, str]]
 Checker = list[tuple[str, tuple[str, int]]]
 
 
@@ -11,55 +10,65 @@ def main():
     rooms = parse("2016/day_04/input.txt")
     part_1 = find_real_rooms(rooms)
     print(part_1)
+    part_2 = decrypt(rooms)
+    print(part_2)
 
 
-def parse(input_file: str) -> RoomCodes:
+def parse(input_file: str) -> list:
     in_file: TextIO
     with open(input_file, encoding="utf-8") as in_file:
         regex: str = r"([a-z-]+)(\d+)(\[[a-z]+\])"
-        input_data: RoomCodes = [findall(regex, line)[0] for line in in_file]
-    rooms: RoomCodes = []
+        input_data: list = [findall(regex, line)[0] for line in in_file]
+    rooms: list = []
     for line in input_data:
         name, room_id, check_sum = line
-        stripped_name = tuple(findall(r"[^-]", name))
-        int_room_id = int(room_id)
+        stripped_name: tuple = tuple(findall(r"[^-]", name))
+        int_room_id: int = int(room_id)
         room = stripped_name, int_room_id, check_sum
         rooms.append(room)
     return rooms
 
 
-def find_real_rooms(rooms: RoomCodes):
-    id_sum = 0
+def find_real_rooms(rooms: list) -> int:
+    id_sum: int = 0
     for room in rooms:
         name, room_id, check_sum = room
         check_sum = check_sum.strip("[]")
-        char_counter = Counter(name)
+        char_counter: Counter = Counter(name)
+        char_counts: list
         char_counts = sorted(char_counter.items(), key=lambda x: x[1], reverse=True)
-        print(char_counts)
         char_counts = [[char, amt] for char, amt in char_counts]
         secondary_sort(char_counts)
-        print(char_counts)
-        letters = "".join([char for char, _ in char_counts])[:5]
+        letters: str = "".join([char for char, _ in char_counts])[:5]
         if letters == check_sum:
             id_sum += room_id
     return id_sum
-            
 
 
-
-def secondary_sort(letters):
+def secondary_sort(letters: list[list]) -> None:
     """Sort the characters with the same count"""
     for idx in range(1, len(letters)):
         char, count = letters[idx]
         if count == letters[idx - 1][1]:
-            pos = idx - 1
+            pos: int = idx - 1
             while 0 <= pos and char < letters[pos][0] and count == letters[pos][1]:
                 letters[pos + 1] = letters[pos]
                 pos -= 1
             letters[pos + 1] = [char, count]
 
-def decrypt(rooms):
-    pass
+
+def decrypt(rooms) -> int:
+    alphabet: deque = deque(ascii_lowercase)
+    for name, room_id, _ in rooms:
+        decrypted_name_lst: list[str] = []
+        for char in name:
+            pos: int = alphabet.index(char)
+            alphabet.rotate(-room_id)
+            decrypted_name_lst.append(alphabet[pos])
+        decrypted_name: str = "".join(decrypted_name_lst)
+        if "northpole" in decrypted_name:
+            return room_id
+    return -1
 
 
 if __name__ == "__main__":
