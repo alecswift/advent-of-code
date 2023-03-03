7ed618b6d99934ca503a0424b3ae49f063e5dc8c
Alec Swift <aleceswift@gmail.com>
2023-03-03 15:37:34 -0800

    Refactor find_bot function

--- 2016/day_10/solution.py
+++ 2016/day_10/solution.py
@@ -10,9 +10,9 @@ def main():
     for bot, vals in bot_vals.items():
         if len(vals) == 2:
             curr_bot = bot_instructions[bot]
-    bot_num, outputs = find_bot(bot_vals, bot_instructions, [curr_bot])
+    bot_num, outputs = find_bot(bot_vals, bot_instructions, [curr_bot], {})
     print(bot_num)
-    print(outputs['0'][0] * outputs['1'][0] * outputs['2'][0])
+    print(outputs["0"][0] * outputs["1"][0] * outputs["2"][0])
 
 
 # first carry out the instructions that initialize bot with
@@ -20,12 +20,13 @@ def main():
 # find bots with two values (have a full bot stack) and execute their instructions checking the values they
 # have until we find the ones we're looking for
 
-class Bot:
 
+class Bot:
     def __init__(self, bot_num, out_nums, out_types):
         self.bot_num = bot_num
         self.out_nums = out_nums
         self.out_types = out_types
+
 
 def parse(input_file: str) -> list[str]:
     in_file: TextIO
@@ -48,9 +49,8 @@ def parse(input_file: str) -> list[str]:
                 bot_instructions[bot_num] = bot
     return bot_vals, bot_instructions
 
-def find_bot(bot_vals, bot_instructions, curr_bots, outputs = None, bot = None):
-    if outputs is None:
-        outputs = {}
+
+def find_bot(bot_vals, bot_instructions, curr_bots, outputs, bot=None):
     curr_bot = curr_bots[0]
     vals = bot_vals[curr_bot.bot_num]
     low_val = min(vals)
@@ -58,38 +58,37 @@ def find_bot(bot_vals, bot_instructions, curr_bots, outputs = None, bot = None):
     low_out, high_out = curr_bot.out_nums
     low_type, high_type = curr_bot.out_types
     next_bot = None
+
     if low_type == "output":
-        if outputs.get(low_out):
-            outputs[low_out].append(low_val)
-        else:
-            outputs[low_out] = [low_val]
+        add_val_to(outputs, low_out, low_val)
     else:
-        if bot_vals.get(low_out):
-            bot_vals[low_out].append(low_val)
-        else:
-            bot_vals[low_out] = [low_val]
+        add_val_to(bot_vals, low_out, low_val)
         if len(bot_vals[low_out]) == 2:
             curr_bots.append(bot_instructions[low_out])
+
     if high_type == "output":
-        if outputs.get(high_out):
-            outputs[high_out].append(high_val)
-        else:
-            outputs[high_out] = [high_val]
+        add_val_to(outputs, high_out, high_val)
     else:
-        if bot_vals.get(high_out):
-            bot_vals[high_out].append(high_val)
-        else:
-            bot_vals[high_out] = [high_val]
+        add_val_to(bot_vals, high_out, high_val)
         if len(bot_vals[high_out]) == 2:
             curr_bots.append(bot_instructions[high_out])
+
     bot_vals[curr_bot.bot_num].clear()
     curr_bots.pop(0)
     next_bot = curr_bots[0]
     if min(bot_vals[next_bot.bot_num]) == 17 and max(bot_vals[next_bot.bot_num]) == 61:
         bot = next_bot
-    if outputs.get('0') and outputs.get('1') and outputs.get('2'):
+    if outputs.get("0") and outputs.get("1") and outputs.get("2"):
         return bot.bot_num, outputs
     return find_bot(bot_vals, bot_instructions, curr_bots, outputs, bot)
+
+
+def add_val_to(a_dict, key, val):
+    if a_dict.get(key) is None:
+        a_dict[key] = [val]
+    else:
+        a_dict[key].append(val)
+
 
 if __name__ == "__main__":
     main()
