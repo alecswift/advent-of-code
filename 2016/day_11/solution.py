5a6fe9ed621180580220e18fb2df8af5dddf872a
Alec Swift <aleceswift@gmail.com>
2023-03-04 20:18:19 -0800

    Make directory day 11

---
+++ 2016/day_11/solution.py
@@ -0,0 +1,16 @@
+from re import findall
+from typing import TextIO
+
+
+def main():
+    pass
+
+def parse(input_file):
+    in_file: TextIO
+    floors = {}
+    with open(input_file, encoding="utf-8") as in_file:
+        for idx, line in in_file:
+            items = 
+
+if __name__ == "__main__":
+    main()
