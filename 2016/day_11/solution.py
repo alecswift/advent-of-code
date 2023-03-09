12ac32de077792dc87a168e5eaaf7199be7310e1
Alec Swift <aleceswift@gmail.com>
2023-03-09 15:59:31 -0800

    Start part 2

--- 2016/day_11/solution.py
+++ 2016/day_11/solution.py
@@ -1,5 +1,5 @@
 from collections import deque
-from itertools import combinations, product
+from itertools import combinations
 from re import findall
 from typing import TextIO
 from copy import deepcopy
@@ -19,11 +19,11 @@ def main():
     target = sum(len(items) for items in floors.values())
     steps = bfs(floors, target)
     print(steps)
-    floors = parse("2016/day_11/input_part_2.txt")
-    # print(floors)
-    # target = sum(len(items) for items in floors.values())
-    # steps = bfs(floors, target)
-    # print(steps)
+    part_2_floors = parse("2016/day_11/input_part_2.txt")
+    part_2_target = sum(len(items) for items in part_2_floors.values())
+    print(part_2_target)
+    part_2_steps = bfs(part_2_floors, part_2_target)
+    print(part_2_steps)
 
 
 def parse(input_file):
@@ -76,6 +76,7 @@ def bfs(floors, target):
     seen = set()
     curr_floor = 1
     root = Node(floors, 1, 0, 1)
+    curr_state = None
     queue = deque([root])
     while len(queue[0].floors[4]) != target:
         curr_node = queue[0]
@@ -85,48 +86,61 @@ def bfs(floors, target):
             continue
         if not curr_node.floors[curr_node.bottom_floor]:
             curr_node.bottom_floor += 1
+
         direction = 1
         curr_floor = curr_node.curr_floor
         if curr_floor != 4:
-            check_items(curr_node, direction, queue)
+            steps = check_items(curr_node, direction, queue, target)
 
         direction = -1
         if curr_floor != curr_node.bottom_floor:
-            check_items(curr_node, direction, queue)
+            steps = check_items(curr_node, direction, queue, target)
+        if steps is not None:
+            return steps
         seen.add((curr_floor, curr_node.steps, curr_state))
         queue.popleft()
     return queue[0].steps
 
 
-def check_items(curr_node, direction, queue):
+def check_items(curr_node, dire, queue, target):
     curr_floor = curr_node.curr_floor
     curr_floors = curr_node.floors
     curr_floor_items = curr_floors[curr_floor]
-    next_floor = curr_floor + direction
+    next_floor = curr_floor + dire
     next_floor_items = curr_floors[next_floor]
     # how to handle two item move?
     double_move_up = False
-    for move in potential_moves(curr_floor_items):
-        if double_move_up and len(move) == 1 and direction == 1:
+    single_move_down = False
+    for move in potential_moves(curr_floor_items, dire):
+        if double_move_up and len(move) == 1 and dire == 1:
+            break
+        if single_move_down and len(move) == 2 and dire == -1:
             break
         new_curr_floor = curr_floor_items.difference(set(move))
         new_next_floor = set(next_floor_items)
         new_next_floor.update(set(move))
         move_is_possible = test_move(new_next_floor) and test_move(new_curr_floor)
         if move_is_possible:
+            if len(move) == 1:
+                single_move_down = True
             if len(move) == 2:
                 double_move_up = True
             new_floors = deepcopy(curr_floors)
             new_floors[curr_floor] = new_curr_floor
             new_floors[next_floor] = new_next_floor
             new_node = Node(new_floors, next_floor, curr_node.steps + 1, curr_node.bottom_floor)
+            if len(new_floors[4]) == target:
+                return new_node.steps
             queue.append(new_node)
 
-def potential_moves(curr_floor_items):
+def potential_moves(curr_floor_items, dire):
     moves = []
+    if dire == -1:
+        moves.extend(combinations(curr_floor_items, 1))
     if 1 < len(curr_floor_items):
         moves.extend(combinations(curr_floor_items, 2))
-    moves.extend(combinations(curr_floor_items, 1))
+    if dire == 1:
+        moves.extend(combinations(curr_floor_items, 1))
     return moves
 
 def test_move(floor_items):
