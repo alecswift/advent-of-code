030b7f028414956c297517fc65a4855279357e65
Alec Swift <aleceswift@gmail.com>
2023-03-09 13:49:44 -0800

    Add part 1 solution

--- 2016/day_11/solution.py
+++ 2016/day_11/solution.py
@@ -1,12 +1,21 @@
 from collections import deque
+from itertools import combinations, product
 from re import findall
 from typing import TextIO
 from copy import deepcopy
 
+# check items function does not work
+# iterates through items that have already been added to nodes
+# Consider changing to finding potential combinations of moves (all 1 or 2 combos)
+# iterate through those, first check if it's a possible move
+# check this by seeing if there is a microchip
+# And comparing it to the floor we're moving to
+# to see if there are any incompatible parts
+# Check the directions again for conditions!
+# then create the new node
 
 def main():
     floors = parse("2016/day_11/input.txt")
-    print(build_state(floors))
     target = sum(len(items) for items in floors.values())
     steps = bfs(floors, target)
     print(steps)
@@ -59,13 +68,17 @@ def bfs(floors, target):
     Breadth first search of paths to get all items
     on the fourth floor
     """
+    seen = set()
     curr_floor = 1
     root = Node(floors, 1, 0, 1)
     queue = deque([root])
     while len(queue[0].floors[4]) != target:
         curr_node = queue[0]
+        curr_state = build_state(curr_node.floors)
+        if (curr_floor, curr_node.steps, curr_state) in seen:
+            queue.popleft()
+            continue
         if not curr_node.floors[curr_node.bottom_floor]:
-            del curr_node.floors[curr_node.bottom_floor]
             curr_node.bottom_floor += 1
         direction = 1
         curr_floor = curr_node.curr_floor
@@ -75,84 +88,67 @@ def bfs(floors, target):
         direction = -1
         if curr_floor != curr_node.bottom_floor:
             check_items(curr_node, direction, queue)
+        seen.add((curr_floor, curr_node.steps, curr_state))
         queue.popleft()
     return queue[0].steps
 
 
 def check_items(curr_node, direction, queue):
-    cache = {} # state = dict[int, set]: move = (direction, items)
     curr_floor = curr_node.curr_floor
     curr_floors = curr_node.floors
     curr_floor_items = curr_floors[curr_floor]
-    # how to handle two item move?
-    for item in curr_floor_items:
-        item_is_microchip = item.imag == 0
-        if item_is_microchip:
-            handle_moves(queue, direction, curr_node, item)
-
-def handle_moves(queue, direction, curr_node, item):
-    curr_floors = curr_node.floors
-    curr_floor = curr_node.curr_floor
     next_floor = curr_floor + direction
-    new_node = move_item(curr_floors, curr_node, next_floor, item)
-    if direction == 1:
-        if new_node is not None:
+    next_floor_items = curr_floors[next_floor]
+    # how to handle two item move?
+    for move in possible_moves(curr_floor_items):
+        new_curr_floor = curr_floor_items.difference(set(move))
+        new_next_floor = set(next_floor_items)
+        new_next_floor.update(set(move))
+        move_is_possible = test_move(new_next_floor) and test_move(new_curr_floor)
+        if move_is_possible:
+            new_floors = deepcopy(curr_floors)
+            new_floors[curr_floor] = new_curr_floor
+            new_floors[next_floor] = new_next_floor
+            new_node = Node(new_floors, next_floor, curr_node.steps + 1, curr_node.bottom_floor)
             queue.append(new_node)
-            new_floors = new_node.floors
-            for item2 in new_floors[curr_floor]:
-                new_node = move_item(new_floors, curr_node, next_floor, item2)
-                if new_node is not None:
-                    queue.append(new_node)
-        if item + 1j in curr_floors[curr_floor]:
-            cp_floors = deepcopy(curr_floors)
-            cp_floors[curr_node.curr_floor].remove(item)
-            cp_floors[next_floor].add(item)
-            cp_floors[curr_node.curr_floor].remove(item + 1j)
-            cp_floors[next_floor].add(item + 1j)
-            new_node = Node(cp_floors, next_floor, curr_node.steps + 1, curr_node.bottom_floor)
-            queue.append(new_node)
-    else:
-        if new_node is not None:
-            queue.append(new_node)
-            new_floors = new_node.floors
+
+def possible_moves(curr_floor_items):
+    moves = []
+    moves.extend(combinations(curr_floor_items, 1))
+    if 1 < len(curr_floor_items):
+        moves.extend(combinations(curr_floor_items, 2))
+    return moves
+
+def test_move(floor_items):
+    gens = [item for item in floor_items if item.imag == 1]
+    if not gens:
+        return True
+    micros = [item for item in floor_items if item.imag == 0]
+    if gens and micros:
+        for micro in micros:
+            ele = micro.real
+            if complex(ele, 1) not in floor_items:
+                return False
+    return True
+
 
 def build_state(curr_floors):
     state = []
     for floor, items in curr_floors.items():
         state_items = []
         for item in items:
-            state_items.append(int(item.imag))
+            ele, gen_or_micro = item.real, item.imag
+            pair_floor = find_floor_pair(curr_floors, ele, gen_or_micro)
+            state_items.append((gen_or_micro, pair_floor))
         state.append((floor, tuple(state_items)))
     return tuple(state)
-    # not correct have to correspond pairs
 
-
-def move_item(curr_floors, curr_node, next_floor, item):
-    """Move item to another floor if possible"""
-    if move_possible(curr_floors, next_floor, item):
-        # handle single move
-        cp_floors = deepcopy(curr_floors)
-        cp_floors[curr_node.curr_floor].remove(item)
-        cp_floors[next_floor].add(item)
-        new_node = Node(cp_floors, next_floor, curr_node.steps + 1, curr_node.bottom_floor)
-        return new_node
-    return None
-
-
-def move_possible(floors, next_floor, item):
-    """Check if a moving an item is possible"""
-    if next_floor not in floors:
-        return False
-
-    ele, gen_or_micro = item.real, item.imag
-    next_floor_items = floors[next_floor]
-    for item_2 in next_floor_items:
-        gen_or_micro_2 = item_2.imag
-        other_type_on_next_floor = gen_or_micro != gen_or_micro_2
-        is_compatible_item = complex(ele, gen_or_micro_2) in next_floor_items
-        if other_type_on_next_floor and not is_compatible_item:
-            return False
-    return True
+def find_floor_pair(curr_floors, ele, gen_or_micro):
+    other_type = 1 if gen_or_micro == 0 else 1
+    for floor, items in curr_floors.items():
+        for item in items:
+            if item.imag == other_type and item.real == ele:
+                return floor
 
 
 if __name__ == "__main__":
