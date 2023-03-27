4dd683c47992b9f3ff572c9f4202ed9d0536ae99
Alec Swift <aleceswift@gmail.com>
2023-03-26 17:34:36 -0700

    Add puzzle explanation to top of file

--- 2016/day_22/solution.py
+++ 2016/day_22/solution.py
@@ -1,4 +1,8 @@
+"""Puzzle explanation: https://adventofcode.com/2016/day/22"""
+
 from __future__ import annotations
+from collections import deque
+from copy import deepcopy
 from re import findall
 from typing import TextIO
 
@@ -6,6 +10,7 @@ def main():
     nodes = parse("2016/day_22/input.txt")
     pairs = find_pairs(nodes)
     print(len(pairs))
+    print(shortest_path())
 
 def parse(input_file: str) -> dict[complex, Node]:
     in_file: TextIO
@@ -15,8 +20,26 @@ def parse(input_file: str) -> dict[complex, Node]:
     for line in input_data.split("\n")[2:]:
         x_coord, y_coord, size, used, avail, use_perc = findall('\d+', line)
         pos = complex(int(x_coord), int(y_coord))
-        nodes[pos] = Node(pos, int(size), int(used), int(avail), int(use_perc))
+        nodes[pos] = Node(int(size), int(used), int(avail), int(use_perc))
     return nodes
+
+def shortest_path():
+    steps = 0
+    start = (17,22) # one empty node
+    mid = (0,13) # only path through y = 13
+    steps += taxicab_distance(start, mid)
+    end = (36, 0)
+    steps += taxicab_distance(mid, end)
+    steps += 1 # move goal one further
+    steps += (36*5) # move goal one further than move empty space around goal 36 times
+    return steps
+
+
+def taxicab_distance(start, end):
+    x_1, y_1 = start
+    x_2, y_2 = end
+    return abs(x_1 - x_2) + abs(y_1 - y_2)
+
 
 def find_pairs(nodes: dict[complex, Node]) -> int:
     pairs = set()
@@ -31,15 +54,14 @@ def find_pairs(nodes: dict[complex, Node]) -> int:
     return pairs
 
 
-
 class Node:
 
-    def __init__(self, pos, size, used, avail, use_perc):
-        self.pos = pos
+    def __init__(self, size, used, avail, use_perc):
         self.size = size
         self.used = used
         self.avail = avail
         self.use_perc = use_perc
+        self.can_receive = False
 
 if __name__ == "__main__":
     main()
