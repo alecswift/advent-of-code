a7cccff92e448197b80a3803796df8614215c4f0
Alec Swift <aleceswift@gmail.com>
2023-03-30 21:19:29 -0700

    Add solutions

---
+++ 2017/day_5/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/alecswift/advent_of_code/util"
+)
+func main() {
+	nums := parse("/home/alec/Desktop/code/advent_of_code/2017/day_5/input.txt")
+	steps1 := traverse(nums, true)
+	nums = parse("/home/alec/Desktop/code/advent_of_code/2017/day_5/input.txt")
+	steps2 := traverse(nums, false)
+	fmt.Print(steps1, "\n", steps2)
+}
+
+func traverse(nums []int, part1 bool) int {
+	var prevIdx int
+	steps := 0
+	idx := 0
+
+	for idx > -1 && idx < len(nums) {
+		
+		if part1 {
+			prevIdx = idx
+			idx += nums[idx]
+			nums[prevIdx]++
+		} else {
+			prevIdx = idx
+			idx += nums[idx]
+			if nums[prevIdx] >= 3 {
+				nums[prevIdx]--
+			} else {
+				nums[prevIdx]++
+			}
+		}
+
+		steps ++
+	}
+
+	return steps
+}
+
+func parse(file_name string) []int {
+	data := util.FileToStr(file_name)
+	splitLines := strings.Split(data, "\n")
+	nums := []int{}
+	
+	for _, line := range splitLines {
+		num, err := strconv.Atoi(line)
+
+		if err != nil {
+			panic("Error")
+		}
+
+		nums = append(nums, num)
+	}
+	
+	return nums
+}
