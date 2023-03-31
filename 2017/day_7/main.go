0d417bfc562f8b8c410ce5e513f2f6e98249d0f7
Alec Swift <aleceswift@gmail.com>
2023-03-31 12:10:19 -0700

    Add parse function

--- 2017/day_7/main.go
+++ 2017/day_7/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/alecswift/advent_of_code/util"
+)
+
+type Node struct {
+	name     string
+	weight   int
+	parent   *Node
+	children []*Node
+}
+
 func main() {
-	panic("nobody")
+	parse("/home/alec/Desktop/code/advent_of_code/2017/day_7/input.txt")
+}
+
+func parse(fileName string) {
+	data := util.FileToStr(fileName)
+	splitLines := strings.Split(data, "\n")
+	nodes := make(map[string]*Node)
+
+	for _, line := range splitLines {
+		re := regexp.MustCompile("[a-z0-9]+")
+		nodeData := re.FindAllString(line, -1)
+		// fmt.Print(nodeData, "\n")
+		isLeaf := len(nodeData) == 2
+
+		if isLeaf {
+			
+		}
+	}
 }
