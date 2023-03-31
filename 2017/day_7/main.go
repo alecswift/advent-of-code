01bc2ed71d67f891f1a2fe05e46a6c40fa0a1bd1
Alec Swift <aleceswift@gmail.com>
2023-03-31 12:16:35 -0700

    Add to parse function

--- 2017/day_7/main.go
+++ 2017/day_7/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"regexp"
 	"strings"
 
@@ -19,6 +20,10 @@ func main() {
 	parse("/home/alec/Desktop/code/advent_of_code/2017/day_7/input.txt")
 }
 
+func findRoot(node *Node) *Node {
+	panic("nobody")
+}
+
 func parse(fileName string) {
 	data := util.FileToStr(fileName)
 	splitLines := strings.Split(data, "\n")
@@ -27,11 +32,27 @@ func parse(fileName string) {
 	for _, line := range splitLines {
 		re := regexp.MustCompile("[a-z0-9]+")
 		nodeData := re.FindAllString(line, -1)
-		// fmt.Print(nodeData, "\n")
 		isLeaf := len(nodeData) == 2
+		name, weightStr := nodeData[0], nodeData[1]
+
+		weight, err := strconv.Atoi(weightStr)
+		if err != nil {
+			panic("Error")
+		}
+
+		fmt.Print(nodeData, "\n")
 
 		if isLeaf {
-			
+			newNode := Node{
+				name: name,
+				weight: weight,
+			}
+			// possibly check for membership in dict first
+			// might not have to do this for leafs
+			// Also look up struct info to see if you need to initialize every data member
+			nodes[name] = &newNode
+		} else {
+			panic("nobody")
 		}
 	}
 }
