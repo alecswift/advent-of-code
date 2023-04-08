2c3d6b7c7bdb2a5c447d177188ebb53f69e619a6
Alec Swift <aleceswift@gmail.com>
2023-04-08 13:37:37 -0700

    Add parse function

--- 2017/day_13/main.go
+++ 2017/day_13/main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"strings"
 
+	"github.com/alecswift/advent_of_code/stringOps"
 	"github.com/alecswift/advent_of_code/util"
 )
 
 func main() {
 	panic("err")
+}
+
+
+func parse(inputFile string) {
+	inputData := util.FileToStr(inputFile)
+	splitLines := strings.Split(inputData, "\n")
+	things := make([][]int, 98)
+
+	for _, line := range splitLines {
+		data := strings.Split(line, ": ")
+		depth := stringOps.strToInt(data[0])
+		fRange := stringOps.strToInt(data[1])
+
+	}
 }
