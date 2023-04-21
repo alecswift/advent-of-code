a27a6049f2dd116a84fbb965835e749403a5923b
Alec Swift <aleceswift@gmail.com>
2023-04-20 21:24:26 -0700

    Add part 2 solution

--- 2017/day_17/main.go
+++ 2017/day_17/main.go
@@ -13,7 +13,10 @@ const Steps = 363
 func main() {
 	initialBuffer := []int{0}
 	part1Sol := part1(initialBuffer)
-	fmt.Print(part1Sol)
+	fmt.Print(part1Sol, "\n")
+
+	part2Sol := part2()
+	fmt.Print(part2Sol)
 }	
 
 func part1(buffer []int) int {
@@ -26,4 +29,24 @@ func part1(buffer []int) int {
 	}
 
 	return buffer[currPos + 1]
+}
+
+func part2() int {
+	var currPos int
+	var zeroPos int
+	var valAfterZero int
+
+	for i := 1; i <= 50000000; i++ {
+		currPos = (currPos + Steps) % i
+		currPos++
+
+		if currPos == zeroPos + 1 {
+			valAfterZero = i
+		}
+		if currPos <= zeroPos {
+			zeroPos++
+		}
+	}
+
+	return valAfterZero
 }
