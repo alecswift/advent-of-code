6b41323c8bba2b5e6851b00efc4c49d01732775a
Alec Swift <aleceswift@gmail.com>
2023-04-20 21:05:55 -0700

    Add part 1 solution

--- 2017/day_17/main.go
+++ 2017/day_17/main.go
@@ -2,6 +2,28 @@
 
 package main
 
+import (
+	"fmt"
+
+	"github.com/alecswift/advent_of_code/arrayOps"
+)
+
+const Steps = 363
+
 func main() {
-	panic("err")
+	initialBuffer := []int{0}
+	part1Sol := part1(initialBuffer)
+	fmt.Print(part1Sol)
+}	
+
+func part1(buffer []int) int {
+	var currPos int
+
+	for i := 1; i <= 2017; i++ {
+		currPos = (currPos + Steps) % len(buffer)
+		currPos++
+		buffer = arrayOps.Insert(buffer, currPos, i)
+	}
+
+	return buffer[currPos + 1]
 }
