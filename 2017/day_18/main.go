93733141a9028ee501f6223a18f718438d2622c1
Alec Swift <aleceswift@gmail.com>
2023-05-06 14:36:28 -0700

    Add sync function

--- 2017/day_18/main.go
+++ 2017/day_18/main.go
@@ -11,16 +11,21 @@ import (
 	"github.com/alecswift/advent_of_code/stringOps"
 )
 
+
+// state: next instruction, registers, queue
+// Go til both programs hit a rcv. Then exit input the correct que # into the rcv regs and repeat
+// while loop continue until a program hits a recieve with no val in queue terminate that one
+// fully end when both programs have terminated
+
 func main() {
 	instructions, registers := parse("/home/alec/Desktop/code/advent-of-code/2017/day_18/input.txt")
-	part1Sol := execute(instructions, registers)
-	fmt.Print(part1Sol)
+	part1Sol := execute(instructions, registers, []int{})
+	fmt.Print(part1Sol[len(part1Sol) - 1])
 
-
+	sync(instructions, registers)
 }
 
-func execute(instructions [][]string, registers map[string]int) int {
-	var prevFreq int
+func execute(instructions [][]string, registers map[string]int, queue []int) []int {
 
 	for idx := 0; idx < len(instructions); idx++ {
 		instr := instructions[idx]
@@ -28,7 +33,7 @@ func execute(instructions [][]string, registers map[string]int) int {
 
 		switch instrName {
 		case "snd":
-			prevFreq = determineVal(instr[1], registers)
+			queue = append(queue, determineVal(instr[1], registers))
 		case "set":
 			reg, val := instr[1], determineVal(instr[2], registers)
 			registers[reg] = val
@@ -43,13 +48,32 @@ func execute(instructions [][]string, registers map[string]int) int {
 			registers[reg] %= val
 		case "rcv":
 			val := determineVal(instr[1], registers)
-			if val != 0 {return prevFreq}
+			if val != 0 {return queue}
 		case "jgz":
 			condVal, jmpVal := determineVal(instr[1], registers), determineVal(instr[2], registers)
 			if condVal > 0 {idx += jmpVal - 1}
 		}
 	}
-	return -1
+	return queue
+}
+
+func sync(instructions [][]string, registers1 map[string]int) {
+	//queue1 := []int{}
+	//queue2 := []int{}
+	registers2 := make(map[string]int)
+
+	for reg := range registers1 {
+		registers1[reg] = 0
+		if reg == "p" {
+			registers2[reg] = 1
+		} else {
+			registers2[reg] = 0
+		}
+	}
+
+	for true {
+
+	}
 }
 
 func determineVal(char string, registers map[string]int) int {
