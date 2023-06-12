47b86cb03cf50d80a10d417c42ba9f106a8e0055
Alec Swift <aleceswift@gmail.com>
2023-06-12 13:07:24 -0700

    Fix bug in parse function

--- 2017/day_21/main.go
+++ 2017/day_21/main.go
@@ -11,22 +11,28 @@ import (
 )
 
 func main() {
-	rules := parse("/home/alec/Desktop/code/advent-of-code/2017/day_21/input.txt")
-	fmt.Print(rules)
+	rules4, rules9 := parse("/home/alec/Desktop/code/advent-of-code/2017/day_21/input.txt")
+	fmt.Print(rules4, "\n", len(rules4))
+	fmt.Print("\n", rules9, "\n", len(rules9))
 }
 
-func parse(inputFile string) map[int]int {
+func parse(inputFile string) (map[int]int, map[int]int) {
 	rawData := util.FileToStr(inputFile)
 	re := regexp.MustCompile(`[.#/]+`)
 	data := re.FindAllString(rawData, -1)
-	rules := make(map[int]int)
+	rules4 := make(map[int]int)
+	rules9 := make(map[int]int)
 
-	for i := 1; i < len(data); i += 2 {
-		match, conversion := strToBits(data[i - 1]), strToBits(data[i])
-		rules[match] = conversion
+	for i := 0; i < len(data); i += 2 {
+		match, conversion := strToBits(data[i]), strToBits(data[i + 1])
+		if len(data[i]) == 5 {
+			rules4[match] = conversion
+		} else {
+			rules9[match] = conversion
+		}
 	}
 
-	return rules
+	return rules4, rules9
 }
 
 func strToBits(str string) int {
