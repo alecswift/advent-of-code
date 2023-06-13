29a6199a0c45ddad179e4522596f9ffada8b40fb
Alec Swift <aleceswift@gmail.com>
2023-06-12 21:30:45 -0700

    Add rotations

--- 2017/day_21/main.go
+++ 2017/day_21/main.go
@@ -5,6 +5,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/bits"
 	"regexp"
 
 	"github.com/alecswift/advent_of_code/util"
@@ -16,12 +17,88 @@ func main() {
 	fmt.Print("\n", rules9, "\n", len(rules9))
 }
 
-func parse(inputFile string) (map[int]int, map[int]int) {
+func split4by4(bits16 uint) []uint {
+	out := []uint{}
+	return out
+}
+
+func flipVert4(bits4 uint) uint {
+	return (bits4 << 2) | (bits4 >> 2)
+}
+
+func flipHor4(bits4 uint) uint {
+	evenMask := 5
+	oddMask := 10
+
+	evenBits := bits4 & uint(oddMask)
+	oddBits := bits4 & uint(evenMask)
+
+	swapped := (evenBits << 1) | (oddBits >> 1)
+
+	return swapped
+}
+
+func flipDiag4(bits4 uint) uint {
+	midBits := bits4 & 6
+	endBits := bits4 & 9
+	out := midBits | bits.Reverse(endBits)
+	return out
+}
+
+func flipVert9(bits9 uint) uint {
+	return (bits9 << 6) | (bits9 & 56)| (bits9 >> 6)
+}
+
+func flipHor9(bits9 uint) uint {
+	return (bits.Reverse(bits9 >> 6) << 6) | (bits.Reverse((bits9 & 56) >> 3) << 3) | bits.Reverse(bits9 & 7)
+}
+
+func flipDiag9(bits9 uint) uint {
+	out := 0
+	if bits9 & 256 != 0 {
+		out += 1
+	}
+	if bits9 & 32 != 0 {
+		out += 2
+	}
+	if bits9 & 4 != 0 {
+		out += 4
+	}
+	if bits9 & 128 != 0 {
+		out += 8
+	}
+	if bits9 & 16 != 0 {
+		out += 16
+	}
+	if bits9 & 2 != 0 {
+		out += 32
+	}
+	if bits9 & 64 != 0 {
+		out += 64
+	}
+	if bits9 & 8 != 0 {
+		out += 128
+	}
+	if bits9 & 1 != 0 {
+		out += 256
+	}
+	return uint(out)
+}
+
+func rotate4(bit4 uint) uint {
+	return flipVert4(flipDiag4(bit4))
+}
+
+func rotate9(bit9 uint) uint {
+	return flipVert9(flipDiag9(bit9))
+}
+
+func parse(inputFile string) (map[uint]uint, map[uint]uint) {
 	rawData := util.FileToStr(inputFile)
 	re := regexp.MustCompile(`[.#/]+`)
 	data := re.FindAllString(rawData, -1)
-	rules4 := make(map[int]int)
-	rules9 := make(map[int]int)
+	rules4 := make(map[uint]uint)
+	rules9 := make(map[uint]uint)
 
 	for i := 0; i < len(data); i += 2 {
 		match, conversion := strToBits(data[i]), strToBits(data[i + 1])
@@ -35,14 +112,14 @@ func parse(inputFile string) (map[int]int, map[int]int) {
 	return rules4, rules9
 }
 
-func strToBits(str string) int {
-	var out int
+func strToBits(str string) uint {
+	var out uint
 	exponent := len(str) - (int(math.Log2(float64(len(str)))))
 
 	for _, char := range str {
 		if char == '/' { continue }
 		if char == '#' {
-			out += int(math.Pow(2, float64(exponent)))
+			out += uint(math.Pow(2, float64(exponent)))
 		}
 		exponent -= 1
 	}
