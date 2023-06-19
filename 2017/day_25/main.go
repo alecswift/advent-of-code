265b12d1a1c344233129081d8111f3096dafec9b
Alec Swift <aleceswift@gmail.com>
2023-06-19 11:18:27 -0700

    Hardcode input

--- 2017/day_25/main.go
+++ 2017/day_25/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"strings"
 
+	"github.com/alecswift/advent_of_code/util"
 )
 
 func main() {
-
+	const InitialState = "A"
+	const Steps = 12667664
+	blueprint := map[string][2][3]int{
+		"A": {{1, 1, 66}, {0, -1, 67}},
+		"B": {{1, -1, 65}, {1, 1, 68}},
+		"C": {{0, -1, 66}, {0, -1, 69}},
+		"D": {{1, 1, 65}, {0, 1, 66}},
+		"E": {{1, -1, 70}, {1, -1, 67}},
+		"F": {{1, 1, 68}, {1, 1, 65}},
+	}
 }
 
 func parse(inputFile string) map[string][2][3]int {
+	data := util.FileToStr(inputFile)
 	
+	blueprint := make(map[string][2][3]int)
+
+	return blueprint
 }
