f9cbc78cf706bec10ef5c8b829db0b6863079330
Alec Swift <aleceswift@gmail.com>
2023-06-20 15:21:58 -0700

    Add solution part 2

--- 2018/day_05/main.go
+++ 2018/day_05/main.go
@@ -9,7 +9,37 @@ import (
 func main() {
 	seq := parse("/home/alec/Desktop/code/advent-of-code/2018/day_05/input.txt")
 	part1Sol := react(seq)
-	fmt.Print(part1Sol)
+	seq2 := parse("/home/alec/Desktop/code/advent-of-code/2018/day_05/input.txt")
+	part2Sol := part2(seq2)
+	fmt.Print(part1Sol, "\n", part2Sol)
+}
+
+func part2(seq []rune) int {
+	var min int
+
+	for i := 65; i < 98; i++ {
+		newSeq := removeChars(seq, rune(i), rune(i + 32))
+		length := react(newSeq)
+		if i == 65 || min > length {
+			min = length
+		}
+	}
+
+	return min
+}
+
+func removeChars(seq []rune, char1, char2 rune) []rune {
+	out := make([]rune, len(seq))
+	copy(out, seq)
+
+	for i := 0; i < len(out); i++ {
+		if out[i] == char1 || out[i] == char2 {
+			out = append(out[:i], out[i + 1:]...)
+			i--
+		}
+	}
+
+	return out
 }
 
 func react(seq []rune) int {
@@ -21,7 +51,7 @@ func react(seq []rune) int {
 		if abs(int(char2) - int(char1)) == 32 {
             seq = append(seq[:idx - 1], seq[idx + 1:]...)
 			
-			if idx > 1 && idx != lastIdx {idx--}
+			if idx != 1 && idx != lastIdx {idx--}
 		} else {
 			idx++
 		}
