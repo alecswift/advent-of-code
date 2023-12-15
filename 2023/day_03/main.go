672aae87efab8e0fab0785ed1d3ca7807d633144
Alec Swift <aleceswift@gmail.com>
2023-12-14 21:37:03 -0800

    Make directory for day 3

---
+++ 2023/day_03/main.go
@@ -0,0 +1,12 @@
+// https://adventofcode.com/2023/day/2
+
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/alecswift/advent_of_code/stringOps"
+	"github.com/alecswift/advent_of_code/util"
+)
