777e5266e0bc36179208d978e04ce1f9372dc43a
Alec Swift <aleceswift@gmail.com>
2023-12-15 17:17:21 -0800

    Add parse function

--- 2023/day_03/main.go
+++ 2023/day_03/main.go
@@ -4,9 +4,31 @@ package main
 
 import (
 	"fmt"
-	"regexp"
 	"strings"
 
-	"github.com/alecswift/advent_of_code/stringOps"
 	"github.com/alecswift/advent_of_code/util"
 )
+
+func main() {
+	data := util.FileToStr("/home/alec/Desktop/code/advent-of-code/2023/day_03/input.txt")
+	grid := parse(data)
+	fmt.Print(grid)
+}
+
+func part1(grid [][]rune) {
+
+	for rowNum, row := range grid {
+		for colNum, val := range grid {
+			
+		}
+	}
+}
+
+func parse(data string) [][]rune {
+	grid := [][]rune{}
+	for _, line := range strings.Split(data, "\n") {
+		grid = append(grid, []rune(line))
+	}
+	return grid
+}
+
